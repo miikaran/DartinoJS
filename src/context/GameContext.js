e86bb76b7d4e9ce29e6a1833d8f7f5736da7bf01
miikaran <42628@edu.raseko.fi>
2024-12-07 00:25:05 +0200

    Added some more data fields to game context

--- src/context/GameContext.js
+++ src/context/GameContext.js
@@ -4,5 +4,14 @@ export const GameDataContext = createContext({
     players: null,
     setPlayers: () => {},
     gameMode: null,
+    setGameMode: () => {},
+    turn: null,
+    setTurn: () => {},
+    legsToPlay: null,
+    setLegsToPlay: () => {},
+    legsToWin: null,
+    setLegsToWin: () => {},
+    gameOn: null,
+    setGameOn: () => {}
 })
 
