f353bfd404e385f90ef1fd036d969476f2bcd1d9
evcnt <elmerivk@proton.me>
2024-12-06 19:52:57 +0200

    Refactor button components to use callback props
    Updated NumberedButton and SpecialButton components to accept an onClick prop, allowing for more flexible handling of click events :)

--- src/components/score-buttons/SpecialButton.jsx
+++ src/components/score-buttons/SpecialButton.jsx
@@ -1,19 +1,18 @@
 import React from "react";
-import ScoreButtonClickHandler from "./ScoreButtonClickHandler";
 
 const specialButtons = ['Double', 'Triple', 'Out', 'Undo'];
 
 /* This component represents "special" score buttons,
  * meaning those which do not a have a numbered representation.
  */
-const SpecialButton = () => {
+const SpecialButton = ({ onClick }) => {
     return (
         <div className="special-buttons">
             {specialButtons.map((specialButtonName, buttonIndex) => (
                 <button
                     key={buttonIndex}
                     className="special-button"
-                    onClick={() => ScoreButtonClickHandler(specialButtonName, true)}
+                    onClick={() => onClick(specialButtonName, true)}
                 >
                     {specialButtonName}
                 </button>
